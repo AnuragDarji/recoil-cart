9b5ae94f75d7a3f3621b79abb0f5636ccfdc8b5a
AnuragDarji <anuragdarji29@gmail.com>
2024-02-09 11:43:01 +0530

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CssBaseline } from '@mui/material';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <CssBaseline/>
+    <RecoilRoot>
     <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
 
